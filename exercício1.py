ce205a0e26e4d40bb84b0b505ce9041e68c739d9
Victor Aldair Quispe Baca <130853409+230305609@users.noreply.github.com>
2023-04-22 16:28:53 -0300

    Add files via upload

---
+++ exercício1.py
@@ -0,0 +1,13 @@
+LQ = input("Digite o valor correspondente ao lado de um quadrado: ") 
+
+y = int(LQ) ** 2 
+
+área = (y)
+
+x = int(LQ) * 4
+
+perímetro = x
+
+print("perímetro:", perímetro, "-", "área:", área)
+
+
